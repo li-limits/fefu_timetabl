637091fcd5d9f538c4f6f82c24e2c8e65849bd6d
Pavel <57826001+zetsuboy@users.noreply.github.com>
2021-12-18 13:50:37 +1000

    Update dbconnect.php

--- backend/dbconnect.php
+++ backend/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
+$host = '185.41.161.185';
 $dbname = 'fefu_timetable';
-$username = 'root';
-$password = '';
+$username = 'web';
+$password = 'y0XgOTslLVNG';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", "$username", "$password");
 //$conn->set_charset("utf8");
